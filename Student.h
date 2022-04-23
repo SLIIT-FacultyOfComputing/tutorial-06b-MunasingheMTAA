2cb890822f5d5edd9b25435356eafc3677643023
M.T.A.A.MUNASINGHE <99812469+MunasingheMTAA@users.noreply.github.com>
2022-04-23 07:31:45 +0000

    Tutorial 6

--- Student.h
+++ Student.h
@@ -1,9 +1,17 @@
 class Student {
-  // private section
+private:
+  int studentId;
+  char name[20];
+
+public:
+  void assignDetails(int no, char sname[]);
+  void display();
+  
+};
+// private section
   //    int studentId
   //    name <- 20 charcters
   
   // public section
   //    assignDetails() method declaration
   //    display() method declaration
-};
